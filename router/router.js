f6863be2b125d8a3bc690312bf43a3e691ea760f
zsecq94 <zsecq94@gmail.com>
2024-01-19 17:53:06 +0900

    feat : 중첩라우팅, 인사말 페이지, 연혁페이지 구현

--- router/router.js
+++ router/router.js
@@ -1,20 +1,14 @@
 $(document).ready(() => {
   const router = () => {
     let route = window.location.hash.substr(2);
-    console.log(window.location.hash);
     if (route == "") route = "main";
-    let fileName = route;
+    let fileName = route.split("/")[0];
     let selector = "#app";
     let filePath = "pages/";
 
-    if (route.includes("about/")) {
-      fileName = route.split("about/")[1];
-      filePath += "about/";
-      selector = "#about-content"; // about 페이지의 중단 부분에 해당하는 셀렉터
-    }
-
     $.get(filePath + fileName + ".html", (data) => {
       $(selector).html(data);
+      window.scrollTo(0, 0);
     });
   };
 
