0cf6b7980aed8b8a3d5fc9fb9305848d2ce58f89
zsecq94 <zsecq94@gmail.com>
2024-01-30 17:11:52 +0900

    feat : tech > AI 빅데이터센터 페이지 구현 완료

--- router/router.js
+++ router/router.js
@@ -2,11 +2,11 @@ $(document).ready(() => {
   const router = () => {
     let route = window.location.hash.substr(2);
     if (route == "") route = "main";
-    let fileName = route.split("/")[0];
+    let fileName = route.split("/");
     let selector = "#app";
     let filePath = "pages/";
 
-    $.get(filePath + fileName + ".html", (data) => {
+    $.get(filePath + fileName[0] + ".html", (data) => {
       $(selector).html(data);
       window.scrollTo(0, 0);
     });
